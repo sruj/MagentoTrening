aaab41b9563ba8a9291f7ab0a17fd6f15f986399
sruj <chryplewiczpawel@wp.pl>
2017-01-15 15:56:29 +0100

    CRUD. pobranie produktu

--- test_usun.php
+++ test_usun.php
@@ -9,14 +9,28 @@ Mage::app();
 
 
 
+/**
+ * 1. ładuję produkt o primaryKey entity_id =450 z tabeli catalog_product_entity
+ *    i mam dostęp do wszystkich atrybutów tego produktu
+ *    np var_dump drukuje 'name' to odbieram to getName()
+*/
+$p = Mage::getModel("catalog/product")->load(450);
+echo $p->getName();
+var_dump($p);
 
-//$product = new Trening_NewModule_Model_Product();
-$product = Mage::getModel("NewModule/Product");
-$product->sayHello();
-
-$cust = Mage::helper("NewModule/Customer");
-$cust->sayHello();
-
-$catal = Mage::getModel("catalog/category")->load(2);
-var_dump($catal->getChildren());
-
+/**
+ * 2. To samo co wyżej ale innym sposobem
+ *    filtruję zapytanie entity_id =450 z tabeli catalog_product_entity
+ *    Nie musze filtrować po  primaryKey  entity_id
+ *    Mogę po każdej kolumnie, ale wtedy mam kolekcję entities i  muszę w pętli.
+ */
+$products_collection = Mage::getModel('catalog/product')
+    ->getCollection()
+    ->addAttributeToSelect('*')
+    ->addFieldToFilter('entity_id','450');
+//    ->addFieldToFilter('price','5.00');
+foreach($products_collection as $product)
+{
+    var_dump($product);
+    echo $product->getName();
+}
