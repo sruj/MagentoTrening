154956222b0316528d27057af2e8652ae6eb23d9
sruj <chryplewiczpawel@wp.pl>
2017-01-19 03:20:29 +0100

    nic

---
+++ app/code/local/Trening/Catalog/Helper/Customer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chiny
+ * Date: 2017-01-12
+ * Time: 20:32
+ */
+
+class Trening_Catalog_Helper_Customer extends Mage_Core_Helper_Abstract{
+    public function sayHello()
+    {
+        echo"c";
+    }
+}
